30108336f00add2ddaabd2b7fa87bb53b72a2750
Omarmoaz10 <52513938+Omarmoaz10@users.noreply.github.com>
2021-09-13 19:36:03 +0200

    initi upload

---
+++ lib/data/data.dart
@@ -0,0 +1,248 @@
+import 'package:facebook_ui/models/models.dart';
+
+final User currentUser = User(
+  name: 'Omar Moaz',
+  imageUrl:
+      'https://wuzzuf.s3.eu-west-1.amazonaws.com/files/user-photos/thumb-33daf396ce2210841403fba10df291ff.jpg',
+);
+
+final List<User> onlineUsers = [
+  User(
+    name: 'Khaled Zaghlola',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: '3Abod',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Ahmed Zakzok',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'HAmoo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Hoda',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'HobA',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Boee',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Farha',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=675&q=80'),
+  User(
+    name: 'Shenoooo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Fatma',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Nada',
+    imageUrl:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Shrok',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Toka',
+    imageUrl:
+        'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+  ),
+  User(
+    name: 'Joo',
+    imageUrl:
+        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+  ),
+  User(
+    name: 'Mostafa',
+    imageUrl:
+        'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
+  ),
+  User(
+    name: 'Omar',
+    imageUrl:
+        'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+  User(
+    name: 'Basem',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519631128182-433895475ffe?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  ),
+  User(
+      name: 'Reham',
+      imageUrl:
+          'https://images.unsplash.com/photo-1515077678510-ce3bdf418862?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=675&q=80'),
+  User(
+    name: 'Habeba',
+    imageUrl:
+        'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80',
+  ),
+  User(
+    name: 'Fares',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  ),
+];
+
+final List<Story> stories = [
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+  Story(
+    user: onlineUsers[2],
+    imageUrl:
+        'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+  ),
+  Story(
+    user: onlineUsers[6],
+    imageUrl:
+        'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[3],
+    imageUrl:
+        'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+  ),
+  Story(
+    user: onlineUsers[9],
+    imageUrl:
+        'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    isViewed: true,
+  ),
+  Story(
+    user: onlineUsers[7],
+    imageUrl:
+        'https://images.unsplash.com/photo-1475688621402-4257c812d6db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
+  ),
+];
+
+final List<Post> posts = [
+  Post(
+    user: currentUser,
+    caption: 'Hi This the my first post',
+    timeAgo: '58m',
+    imageUrl:
+        'https://desktopwallpaperdownload.files.wordpress.com/2012/03/nature-lake-full-hd-wallpaper.jpg',
+    likes: 1202,
+    comments: 184,
+    shares: 96,
+  ),
+  Post(
+    user: onlineUsers[5],
+    caption:
+        'Please enjoy this placeholder text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '3hr',
+    imageUrl: null,
+    likes: 683,
+    comments: 79,
+    shares: 18,
+  ),
+  Post(
+    user: onlineUsers[4],
+    caption: 'This is a very good boi.',
+    timeAgo: '8hr',
+    imageUrl:
+        'https://desktopwallpaperdownload.files.wordpress.com/2012/01/amazing-full-hd-wallpaper-cave-on-the-beach-wallpaper.jpg',
+    likes: 894,
+    comments: 201,
+    shares: 27,
+  ),
+  Post(
+    user: onlineUsers[3],
+    caption: 'Adventure 🏔',
+    timeAgo: '15hr',
+    imageUrl:
+        'https://desktopwallpaperdownload.files.wordpress.com/2012/01/full-hd-country-sunset-amazing-wallpaper.jpg',
+    likes: 722,
+    comments: 183,
+    shares: 42,
+  ),
+  Post(
+    user: onlineUsers[0],
+    caption:
+        'More placeholder text for the soul: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+  Post(
+    user: onlineUsers[9],
+    caption: 'A classic.',
+    timeAgo: '1d',
+    imageUrl:
+        'https://desktopwallpaperdownload.files.wordpress.com/2012/02/nature-rocks-cost-water-amazing-full-hd-wallpaper.jpg',
+    likes: 1523,
+    shares: 129,
+    comments: 301,
+  ),
+  Post(
+    user: onlineUsers[7],
+    caption:
+        'More placeholder text for the soul: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+  Post(
+    user: onlineUsers[2],
+    caption:
+        'More placeholder text for the soul: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '1d',
+    imageUrl: null,
+    likes: 482,
+    comments: 37,
+    shares: 9,
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:facebook_ui/config/palette.dart';
+import 'package:facebook_ui/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FaceBook',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Palette.scaffold),
+      home: NavScreen(),
+    );
+  }
+}
